7ae50b409f875eb38fc20f1c4410099139983f10
orel <orel.avr95@gmail.com>
2020-01-13 16:07:22 +0200

    Orel - almost fixed goFetch

--- src/main/java/edu/greenblitz/bigRodika/commands/chassis/GoFetch.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/chassis/GoFetch.java
@@ -14,44 +14,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GoFetch extends GBCommand {
-    private final double MAX_LIN_V = 1, MAX_ANG_V = 0.25, MAX_LIN_A = 1, MAX_ANG_A = 0.125, JMP = 0.0001;
+    private final double MAX_LIN_V = 0.5, MAX_ANG_V = 0.25, MAX_LIN_A = 0.5, MAX_ANG_A = 0.125, JMP = 0.0001;
     private Point target; // should be gotten by the network tables
     private MotionProfile2D profile2D;
     private PidFollower2D follower2D;
-    private double t0;
+    private double power;
+    private Vector2D v;
 
     public GoFetch(Point target) {
         super(Chassis.getInstance());
-        System.out.println("Ctor");
+        power = 0.3;
         this.target = target;
-        System.out.println("got a target (1,1) ");
         List<State> locations = new ArrayList<>();
-        System.out.println("crested a locations list");
         locations.add(new State(0, 0, 0));
         locations.add(new State(target.getX(), target.getY(), 0, 0, 0));
-        System.out.println("added the locations to the list");
+       // System.out.println("locations ");
         profile2D = ChassisProfiler2D.generateProfile(locations, JMP, MAX_LIN_V, MAX_ANG_V, MAX_LIN_A, MAX_ANG_A, 0, 1, 800);
+       // System.out.println("profile2D");
         follower2D = new PidFollower2D(1,1,1,1, RobotMap.BigRodika.Chassis.WHEEL_DIST, profile2D);
-        System.out.println("generated a profile");
+        //System.out.println("follower2D");
+        System.out.println(profile2D.getTEnd());
     }
 
     @Override
     public void initialize() {
-        System.out.println("init");
-        t0 = System.currentTimeMillis();
+
+        follower2D.init();
     }
 
     @Override
-    public void execute() {
-        System.out.println("execute");
-        double currT = (t0 - System.currentTimeMillis()) / 1000.0;
-        Vector2D v = follower2D.run(Chassis.getInstance().getLeftRate(), Chassis.getInstance().getRightRate(), currT);
-        Chassis.getInstance().tankDrive(v.getX(), v.getY());
-        System.out.println("following");
+        public void execute() {
+        Vector2D v = follower2D.run(Chassis.getInstance().getLeftRate(), Chassis.getInstance().getRightRate(),Chassis.getInstance().getAngularVelocityByWheels());//, currT
+        Chassis.getInstance().arcadeDrive(clamp(v.getX()), clamp(v.getY()));
+        System.out.println(profile2D.getTEnd());
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return follower2D.isFinished();
+    }
+
+    private double clamp(double inp){
+        return Math.copySign(Math.min(Math.abs(inp), 1.0), inp) * power;
     }
 }
