fdf9be44c27327c7de0f5ce3dcf414ab9adab7cd
TheOnion313 <royn77777@gmail.com>
2020-01-15 14:28:14 +0200

    Roie - - temporary coomit Peleg wanna work on my computer

--- src/main/java/edu/greenblitz/bigRodika/Robot.java
+++ src/main/java/edu/greenblitz/bigRodika/Robot.java
@@ -3,8 +3,13 @@ package edu.greenblitz.bigRodika;
 import edu.greenblitz.bigRodika.commands.chassis.ArcadeDrive;
 import edu.greenblitz.bigRodika.commands.chassis.LocalizerCommandRunner;
 import edu.greenblitz.bigRodika.subsystems.Chassis;
+import edu.greenblitz.bigRodika.utils.VisionMaster;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.util.Arrays;
 
 public class Robot extends TimedRobot {
 
@@ -13,6 +18,7 @@ public class Robot extends TimedRobot {
         OI.getInstance();
         Chassis.getInstance();
         CommandScheduler.getInstance().enable();
+        VisionMaster.getInstance();
     }
 
     @Override
@@ -25,5 +31,7 @@ public class Robot extends TimedRobot {
         Chassis.getInstance().toBrake();
         new LocalizerCommandRunner().schedule();
         new ArcadeDrive(Chassis.getInstance(), OI.getInstance().getMainJoystick()).schedule();
+        VisionMaster.getInstance().update();
+        System.out.println(Arrays.toString(VisionMaster.getInstance().getCurrentVisionData()));
     }
 }
