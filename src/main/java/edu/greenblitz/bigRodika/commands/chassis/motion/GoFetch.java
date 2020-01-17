4a58957b01f1b7f5a68f948ad9abe2c4f8a347d3
orel <orel.avr95@gmail.com>
2020-01-17 14:42:41 +0200

    Orel - ended solving merging conflicts

--- src/main/java/edu/greenblitz/bigRodika/commands/chassis/motion/GoFetch.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/chassis/motion/GoFetch.java
@@ -23,7 +23,6 @@ public class GoFetch extends GBCommand {
 
         List<State> locations = new ArrayList<>();
         locations.add(new State(0, 0, 0));
-<<<<<<< HEAD:src/main/java/edu/greenblitz/bigRodika/commands/chassis/GoFetch.java
         locations.add(new State(target.getX(), target.getY(), angle, 0, 0));
 
         ProfilingData data =  RobotMap.BigRodika.Chassis.MotionData.POWER.get("0.7");
@@ -36,15 +35,7 @@ public class GoFetch extends GBCommand {
                 new PIDObject(0*data.getMaxAngularVelocity(), 0, 0*data.getMaxAngularAccel()), 0.01*data.getMaxAngularVelocity(),
                 false);
         prof.setSendData(true);
-=======
-        locations.add(new State(target.getX(), target.getY(), 0, 0, 0));
-       // System.out.println("locations ");
-        profile2D = ChassisProfiler2D.generateProfile(locations, JMP, MAX_LIN_V, MAX_ANG_V, MAX_LIN_A, MAX_ANG_A, 0, 1, 800);
-       // System.out.println("profile2D");
-        follower2D = null;
-        //System.out.println("follower2D");
-        System.out.println(profile2D.getTEnd());
->>>>>>> e9d96835e6998acaa814b8aca397bf7da50d9db8:src/main/java/edu/greenblitz/bigRodika/commands/chassis/motion/GoFetch.java
+
     }
 
     @Override
