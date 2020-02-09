7543f0628fceff657c3d213c73990cd656773daa
Alexey <theemailreader300@gmail.com>
2020-02-09 18:56:42 +0200

    Added full intake command

--- src/main/java/edu/greenblitz/bigRodika/commands/complex/SimpleIntake.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/complex/SimpleIntake.java
@@ -1,6 +1,14 @@
 package edu.greenblitz.bigRodika.commands.complex;
 
+import edu.greenblitz.bigRodika.commands.intake.extender.ExtendRoller;
+import edu.greenblitz.bigRodika.commands.intake.extender.RetractRoller;
+import edu.greenblitz.bigRodika.commands.intake.roller.RollByConstant;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class SimpleIntake extends ParallelCommandGroup {
+public class SimpleIntake extends SequentialCommandGroup {
+    public SimpleIntake(double power) {
+        addCommands(new ParallelCommandGroup(new RollByConstant(power), new ExtendRoller()),
+                new RetractRoller());
+    }
 }
