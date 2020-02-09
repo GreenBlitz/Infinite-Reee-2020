34c1d8d183a69893aeee434249e363122a86882d
Alexey <theemailreader300@gmail.com>
2020-02-09 20:41:20 +0200

    Wifi - Added git interface for turret

--- src/main/java/edu/greenblitz/bigRodika/subsystems/Turret.java
+++ src/main/java/edu/greenblitz/bigRodika/subsystems/Turret.java
@@ -1,12 +1,17 @@
 package edu.greenblitz.bigRodika.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANPIDController;
 import edu.greenblitz.bigRodika.RobotMap;
 import edu.greenblitz.gblib.encoder.IEncoder;
 import edu.greenblitz.gblib.encoder.TalonEncoder;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.greenblitz.motion.pid.PIDObject;
 
 public class Turret implements Subsystem {
     private static Turret instance;
@@ -40,6 +45,7 @@ public class Turret implements Subsystem {
     }
 
     public void moveTurret(double power){
+        SmartDashboard.putNumber("Turret::power", power);
         motor.set(power);
     }
 
@@ -50,5 +56,29 @@ public class Turret implements Subsystem {
     public boolean isSwitchPressed(){
         return microSwitch.get();
     }
+
+    public void moveInserterByPID(double target){
+        motor.set(ControlMode.Velocity, target);
+    }
+
+    public double getInserterSpeed() {
+        return encoder.getNormalizedVelocity();
+    }
+
+    public double getAbsoluteInserterSpeed(){
+        return Math.abs(getInserterSpeed());
+    }
+
+    public void configurePID(int pidIndex, PIDObject obj){
+        motor.config_kP(pidIndex, obj.getKp(), 20);
+        motor.config_kI(pidIndex, obj.getKi(), 20);
+        motor.config_kD(pidIndex, obj.getKd(), 20);
+        motor.config_kF(pidIndex, obj.getKf(), 20);
+    }
+
+    public void selectPIDLoop(int pidIndex){
+        motor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, pidIndex, 20);
+    }
+
 
 }
