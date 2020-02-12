5c97072b5d229ef2c3181d4c037b5a5e0e0f41f7
Alexey <theemailreader300@gmail.com>
2020-02-12 17:49:10 +0200

    Oops didnt fix everything

--- src/main/java/edu/greenblitz/bigRodika/OI.java
+++ src/main/java/edu/greenblitz/bigRodika/OI.java
@@ -1,38 +1,20 @@
 package edu.greenblitz.bigRodika;
 
-<<<<<<< HEAD
 import edu.greenblitz.bigRodika.commands.chassis.motion.PreShoot;
-=======
 import edu.greenblitz.bigRodika.commands.chassis.driver.ArcadeDrive;
-import edu.greenblitz.bigRodika.commands.chassis.profiling.Follow2DProfileCommand;
-import edu.greenblitz.bigRodika.commands.dome.ApproachSwiftly;
+
 import edu.greenblitz.bigRodika.commands.funnel.InsertIntoShooter;
->>>>>>> dev
 import edu.greenblitz.bigRodika.commands.funnel.inserter.InsertByConstant;
 import edu.greenblitz.bigRodika.commands.funnel.inserter.StopInserter;
 import edu.greenblitz.bigRodika.commands.funnel.pusher.PushByConstant;
 import edu.greenblitz.bigRodika.commands.funnel.pusher.StopPusher;
-<<<<<<< HEAD
 import edu.greenblitz.bigRodika.commands.intake.extender.ToggleExtender;
 import edu.greenblitz.bigRodika.commands.intake.roller.RollByConstant;
-=======
 import edu.greenblitz.bigRodika.commands.shooter.pidshooter.threestage.FullyAutoThreeStage;
-import edu.greenblitz.bigRodika.commands.shooter.pidshooter.threestage.ThreeStageShoot;
->>>>>>> dev
+
 import edu.greenblitz.bigRodika.commands.shooter.StopShooter;
-import edu.greenblitz.bigRodika.commands.shooter.pidshooter.TwoStageShoot;
 import edu.greenblitz.gblib.hid.SmartJoystick;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-<<<<<<< HEAD
-=======
-import org.greenblitz.motion.base.State;
-import org.greenblitz.motion.pid.PIDObject;
-import org.greenblitz.motion.profiling.ProfilingData;
-import org.greenblitz.motion.tolerance.AbsoluteTolerance;
-
-import java.util.ArrayList;
-import java.util.List;
->>>>>>> dev
 
 public class OI {
     private static OI instance;
@@ -79,7 +61,6 @@ public class OI {
                 ParallelCommandGroup(new PushByConstant(-0.7), new InsertByConstant(-0.6)));
         secondStick.Y.whenReleased(new ParallelCommandGroup(new StopPusher(), new StopInserter()));
 
-        mainJoystick.START.whenPressed(new CheckMaxRot(0.5));
 
     }
 
