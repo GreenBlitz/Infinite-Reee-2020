ea44751aa44a2f959e98d12f5cb354672b3c6006
orel <orel.avr95@gmail.com>
2020-02-14 14:35:01 +0200

    Look at the last commit

--- src/main/java/edu/greenblitz/bigRodika/commands/complex/autonomous/Trench8BallAuto.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/complex/autonomous/Trench8BallAuto.java
@@ -7,12 +7,15 @@ import edu.greenblitz.bigRodika.commands.funnel.InsertIntoShooter;
 import edu.greenblitz.bigRodika.commands.funnel.pusher.StopPusher;
 import edu.greenblitz.bigRodika.commands.intake.roller.StopRoller;
 import edu.greenblitz.bigRodika.commands.shooter.pidshooter.threestage.autonomous.ThreeStageForAutonomous;
+import edu.greenblitz.bigRodika.subsystems.Chassis;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import org.greenblitz.motion.base.State;
 
 public class Trench8BallAuto extends ParallelCommandGroup {
+
+
 
     public Trench8BallAuto(){
 
@@ -25,13 +28,22 @@ public class Trench8BallAuto extends ParallelCommandGroup {
                         new GoFetch(new State(0, -0.9, 0)),
                         new PreShoot(6.4),
                         new InsertIntoShooter(0.5, 0.7),
-                        new WaitCommand(5)
-//                        new StopRoller(),
-//                        new StopPusher()
+                        new WaitCommand(5),
+                        new StopRoller(),
+                        new StopPusher(),
 
+                        new GoFetch(new State(-Chassis.getInstance().getLocation().getX(), 4.6-Chassis.getInstance().getLocation().getY(),0)),
+                        new GoFetch(new State (0,1.6,0)),
+                        new PreShoot(6.4),
+                        new InsertIntoShooter(0.5, 0.7),
+                        new WaitCommand(5),
+                        new StopRoller(),
+                        new StopPusher()
                 )
 
         );
+
+
 
     }
 
