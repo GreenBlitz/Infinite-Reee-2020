ee1b7a9859460625ebd281c4833fcd044f3e4d97
Peleg <peleg.caduri@gmail.com>
2020-02-19 17:43:24 +0200

    Peleg - adjusted turn to vision to work with turret

--- src/main/java/edu/greenblitz/bigRodika/commands/chassis/motion/PreShoot.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/chassis/motion/PreShoot.java
@@ -12,11 +12,7 @@ import java.util.List;
 
 public class PreShoot extends SequentialCommandGroup {
 
-    public PreShoot() {
-        this(6.0);
-    }
-
-    public PreShoot(double radius) {
+    public PreShoot(double radius, boolean useTurret) {
         HexAlign hexAlign = new HexAlign(radius, 0.2, 0.5, 0.1, 0.5);
         addCommands(hexAlign,
 
@@ -25,10 +21,10 @@ public class PreShoot extends SequentialCommandGroup {
                 new TurnToVision(VisionMaster.Algorithm.HEXAGON,
                         RobotMap.Limbo2.Chassis.MotionData.POWER.get("0.5").getMaxAngularVelocity(),
                         RobotMap.Limbo2.Chassis.MotionData.POWER.get("0.5").getMaxAngularAccel(),
-                        0.5, hexAlign));
+                        0.5, useTurret, hexAlign));
     }
 
-    public PreShoot(List<Double> radsAndCritPoints){
+    public PreShoot(List<Double> radsAndCritPoints, boolean useTurret){
         HexAlign hexAlign = new HexAlign(radsAndCritPoints, 0.2, 0.5, 0.1, 0.5);
         addCommands(hexAlign,
 
@@ -37,7 +33,7 @@ public class PreShoot extends SequentialCommandGroup {
                 new TurnToVision(VisionMaster.Algorithm.HEXAGON,
                         RobotMap.Limbo2.Chassis.MotionData.POWER.get("0.5").getMaxAngularVelocity(),
                         RobotMap.Limbo2.Chassis.MotionData.POWER.get("0.5").getMaxAngularAccel(),
-                        0.5, hexAlign));
+                        0.5, useTurret, hexAlign));
     }
 }
 
