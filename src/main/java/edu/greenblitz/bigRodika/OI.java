e695abb377c60c1ca256de7b0dbd035daa1cd865
GBSoftwareLaptop <you@example.com>
2020-03-03 20:41:55 +0200

    Offical buttons more offical now

--- src/main/java/edu/greenblitz/bigRodika/OI.java
+++ src/main/java/edu/greenblitz/bigRodika/OI.java
@@ -5,6 +5,7 @@ import edu.greenblitz.bigRodika.commands.chassis.motion.PreShoot;
 import edu.greenblitz.bigRodika.commands.chassis.motion.PreShootAndWait;
 import edu.greenblitz.bigRodika.commands.chassis.profiling.Follow2DProfileCommand;
 import edu.greenblitz.bigRodika.commands.complex.multisystem.CompleteShoot;
+import edu.greenblitz.bigRodika.commands.complex.multisystem.ShootAdjesant;
 import edu.greenblitz.bigRodika.commands.dome.DomeApproachSwiftly;
 import edu.greenblitz.bigRodika.commands.dome.DomeMoveByConstant;
 import edu.greenblitz.bigRodika.commands.dome.ResetDome;
@@ -47,8 +48,8 @@ public class OI {
         secondStick = new SmartJoystick(RobotMap.Limbo2.Joystick.SIDE,
                 RobotMap.Limbo2.Joystick.SIDE_DEADZONE);
 
-        initTestButtons();
-//        initOfficalButtons();
+//        initTestButtons();
+        initOfficalButtons();
     }
 
 
@@ -121,43 +122,20 @@ public class OI {
 
     private void initOfficalButtons() {
 
-//        mainJoystick.R1.whileHeld(new ChainFetch(5, mainJoystick));
-//        mainJoystick.R1.whenReleased(new ArcadeDrive(mainJoystick));
-
-        List<Double> rads = new ArrayList<>();
-
-        rads.add(0.5); rads.add(4.0); rads.add(4.5); rads.add(6.3);
-
-        mainJoystick.A.whileHeld(
-                new PreShootAndWait(
-                        new PreShoot(
-                                new DumbAlign(rads, .1, .3))));
-        mainJoystick.A.whenReleased(new ParallelCommandGroup(new ResetDome(), new StopTurret()));
 //        mainJoystick.L1.whenReleased(new ToggleShift());
 
-        mainJoystick.B.whileHeld(new TurretByVision(VisionMaster.Algorithm.HEXAGON));
-//        mainJoystick.B.whenReleased(new StopTurret());
+        mainJoystick.X.whenPressed(new ResetDome(-0.3));
 
-        mainJoystick.X.whenPressed(new ParallelCommandGroup(
-                new ResetDome(-0.3), new ExtendRoller()
-        ));
+        mainJoystick.Y.whenPressed(new ShootAdjesant(mainJoystick.Y));
 
 //        mainJoystick.START.whenPressed(new ToSpeed());
 //        mainJoystick.BACK.whenPressed(new ToPower());
 
-        mainJoystick.POV_UP.whenPressed(new DomeApproachSwiftly(RobotMap.Limbo2.Dome.DOME.get(6.3)));
-        mainJoystick.POV_DOWN.whenPressed(new TurretApproachSwiftlyRadians(0));
-
         // ---------------------------------------------------------------
 
-//        secondStick.R1.whenPressed(new CompleteShoot()); // 1400 = old
-        secondStick.R1.whenPressed(new FullyAutoThreeStage(1650));
-        secondStick.R1.whenReleased(new ParallelCommandGroup(new StopShooter()));
-//                                                             new ResetDome()));
-
-//        secondStick.R1.whenPressed(new FullyAutoThreeStage(2600, 0.5));
-//        secondStick.R1.whenReleased(new ParallelCommandGroup(new StopShooter(),
-//                new ResetDome()));
+        secondStick.R1.whenPressed(new CompleteShoot());
+        secondStick.R1.whenReleased(new ParallelCommandGroup(new StopShooter(),
+                                                             new ResetDome()));
 
         secondStick.L1.whileHeld(new InsertIntoShooter(1.0, 0.8, 0.6));
         secondStick.L1.whenReleased(new ParallelCommandGroup(new StopPusher(),
@@ -173,32 +151,8 @@ public class OI {
         secondStick.A.whileHeld(new RollByConstant(1.0));
         secondStick.R3.whileHeld(new RollByConstant(-0.7));
 
-        List<State> goBack = new ArrayList<>();
-        goBack.add(new State(0, 0));
-        goBack.add(new State(0,0.4));
-
-        secondStick.START.whenPressed(
-
-                new ParallelRaceGroup(
-                        new DomeApproachSwiftly(0.1), // was 0.1
-                        new ThreadedCommand(
-                                new Follow2DProfileCommand(goBack,
-                                        RobotMap.Limbo2.Chassis.MotionData.CONFIG,
-                                        0.3, false),
-                                Chassis.getInstance())
-                ).andThen(
-                        new ExtendRoller()
-                )
-                        .andThen(
-                                new GBCommand() {
-                                    @Override
-                                    public boolean isFinished() {
-                                        return !mainJoystick.A.get();
-                                    }
-                                }
-                        )
-
-        );
+        secondStick.START.whenPressed(new FullyAutoThreeStage(1650));
+        secondStick.START.whenReleased(new ParallelCommandGroup(new StopShooter()));
 
         secondStick.X.whileHeld(new TurretByVision(VisionMaster.Algorithm.HEXAGON));
 
