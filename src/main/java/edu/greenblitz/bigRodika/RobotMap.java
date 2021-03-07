8008122c313366a39ea168cc7a7718510b0737ff
GBSoftwareLaptop <you@example.com>
2021-03-07 14:51:52 +0200

    this is the last change asaf and raz did from tohna computer ~orLAvr

--- src/main/java/edu/greenblitz/bigRodika/RobotMap.java
+++ src/main/java/edu/greenblitz/bigRodika/RobotMap.java
@@ -187,7 +187,7 @@ public class RobotMap {
             public static class MotionData { // TODO: calibrate this
 
                 public static final ProfilingConfiguration CONFIG = new ProfilingConfiguration(
-                        0.85, 1.0, .01,
+                        0.85, 1.0, .05,
                         0.8, 0.0, 2.0, .01,
                         0.5 * 0, 0, 0, .01, 500);
                 public static HashMap<String, ProfilingData> POWER;
