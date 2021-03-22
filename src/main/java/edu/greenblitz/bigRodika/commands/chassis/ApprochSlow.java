169b023397b2239131cb3bfb2ebb5d3206ccfc23
Raz Rishoni <razrishoni@Razs-MacBook-Pro.local>
2021-03-22 16:39:58 +0200

    Raz and Ittai - we changed the code to use the localizer for testing becuse vison is not ready yet.

--- src/main/java/edu/greenblitz/bigRodika/commands/chassis/ApprochSlow.java
+++ src/main/java/edu/greenblitz/bigRodika/commands/chassis/ApprochSlow.java
@@ -2,11 +2,14 @@ package edu.greenblitz.bigRodika.commands.chassis;
 
 import edu.greenblitz.bigRodika.subsystems.Chassis;
 import edu.greenblitz.bigRodika.utils.VisionMaster;
+import org.greenblitz.motion.Localizer;
 
-public class ApprochSlow extends ChassisCommand{
+public class ApprochSlow extends ChassisCommand{ //TODO: make vision work
     private double targetD;
     private double power;
     private double epsilon;
+    private localizer localizer;
+
 
     public ApprochSlow(double targetD){
         this(targetD,0.1,0.1);
@@ -19,17 +22,19 @@ public class ApprochSlow extends ChassisCommand{
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        l = Localizer.getInstance();
+    }
 
     @Override
     public void execute() {
-        double currD = VisionMaster.getInstance().getVisionLocation().getFullDistance();
+        //double currD = VisionMaster.getInstance().getVisionLocation().getFullDistance();
+        double currD = l.getLocation(); //we use the localizer for the testing
         if(targetD < currD){
             Chassis.getInstance().moveMotors(this.power,this.power);
         }else{
             Chassis.getInstance().moveMotors(-this.power,-this.power);
         }
-
     }
 
     @Override
