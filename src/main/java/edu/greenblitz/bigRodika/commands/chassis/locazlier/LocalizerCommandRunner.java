dabc2d950916b8e1da589c6904a0b713f9aaedfe
GBSoftware Laptop <greenblitz4590@gmail.com>
2022-01-26 16:43:59 +0200

    Leftover code, please please commit when you finish working

--- src/main/java/edu/greenblitz/bigRodika/commands/chassis/locazlier/LocalizerCommandRunner.java
+++
@@ -1,9 +0,0 @@
-package edu.greenblitz.bigRodika.commands.chassis.locazlier;
-
-import edu.greenblitz.gblib.threading.ThreadedCommand;
-
-public class LocalizerCommandRunner extends ThreadedCommand {
-    public LocalizerCommandRunner() {
-        super(new LocalizerCommand(20));
-    }
-}
